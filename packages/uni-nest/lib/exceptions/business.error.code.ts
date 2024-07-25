a49fbeb7669fd31ff107975a271902d39eab8e99
hacxy <hacxy.js@outlook.com>
2024-07-25 14:47:28 +0800

    refactor: 移除无关状态码

--- packages/uni-nest/lib/exceptions/business.error.code.ts
+++ packages/uni-nest/lib/exceptions/business.error.code.ts
@@ -11,31 +11,7 @@ export const BUSINESS_ERROR_CODE = {
    * 无效或过期token
    */
   INVALID_TOKEN: {
-    code: 401,
+    code: 1401,
     msg: '无效身份或身份已过期'
-  },
-
-  /**
-   * 禁止访问
-   */
-  ACCESS_FORBIDDEN: {
-    code: 403,
-    msg: '抱歉哦，您无此权限！'
-  },
-
-  /**
-   * 字段错误
-   */
-  FIELD_INCORRECT: {
-    code: 1001,
-    msg: '字段不合法'
-  },
-
-  /**
-   * 数据已被保护
-   */
-  DATA_PROTECTED: {
-    code: 1005,
-    msg: '该数据已被保护'
   }
 };
--- packages/uni-nest/lib/exceptions/business.exceptions.ts
+++ packages/uni-nest/lib/exceptions/business.exceptions.ts
@@ -25,41 +25,9 @@ export class UniBusinessException extends HttpException {
   }
 
   /**
-   * 无权限
-   */
-  static throwForbidden() {
-    throw new UniBusinessException(BUSINESS_ERROR_CODE.ACCESS_FORBIDDEN);
-  }
-
-  /**
-   * 字段不合法
-   * @param msg
-   */
-  static throwFieldsIncorrect(msg: string = '字段不合法') {
-    throw new UniBusinessException({
-      code: BUSINESS_ERROR_CODE.FIELD_INCORRECT.code,
-      msg
-    });
-  }
-
-  /**
    * 无效token或已过期
    */
   static throwInvalidToken() {
     throw new UniBusinessException(BUSINESS_ERROR_CODE.INVALID_TOKEN);
   }
-
-  /**
-   * 用户不存在
-   */
-  // static throwUserNotExist() {
-  //   throw new BusinessException(BUSINESS_ERROR_CODE.USER_DOES_NOT_EXIST);
-  // }
-
-  /**
-   * 数据已被保护
-   */
-  // static throwDataProtected() {
-  //   throw new BusinessException(BUSINESS_ERROR_CODE.DATA_PROTECTED);
-  // }
 }
